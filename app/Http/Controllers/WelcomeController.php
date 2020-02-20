ada1a3984f95bfe964f81f32ba84b5beaadf99dc
imsidz <imsidzluv@gmail.com>
2020-02-21 04:40:14 +0530

    increase products listing

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -12,7 +12,7 @@ class WelcomeController extends Controller
 {
     public function index()
     {
-        $latests = Product::latest()->approved()->store()->paginate('8');
+        $latests = Product::latest()->approved()->store()->paginate('18');
         $banners = Banner::where('active', true)->latest()->paginate(5);
         $subcategories = SubCategory::latest()->whereNotNull('image')->paginate(6);
         $collection1 = Product::inRandomOrder()->approved()->store()->paginate(2);
