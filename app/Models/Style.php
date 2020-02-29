4408ea6a872358b60169d240e17e97dd0d81e999
imsidz <imsidzluv@gmail.com>
2020-03-01 04:33:39 +0530

    fix style error

--- app/Models/Style.php
+++ app/Models/Style.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Style extends Model
 {
     use SoftDeletes;
+
+    public function products()
+    {
+        return $this->hasMany(Product::class, 'style_id', 'id');
+    }
 }
