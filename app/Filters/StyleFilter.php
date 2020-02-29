4a8733707bee251531eaa2b3b1a66de87f1c4c8f
imsidz <imsidzluv@gmail.com>
2020-03-01 04:48:39 +0530

    test

--- app/Filters/StyleFilter.php
+++ app/Filters/StyleFilter.php
@@ -9,6 +9,7 @@ class StyleFilter
     public function filter($builder, $value)
     {
         return $builder->whereHas('style', function ($query) use ($value) {
+            dd($value);
             $query->where('name', 'like', $value);
         });
     }
