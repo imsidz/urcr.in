76b045d8eb17a8c654f602f41dc2fc73e1637728
imsidz <imsidzluv@gmail.com>
2020-03-01 04:55:52 +0530

    removed test

--- app/Filters/StyleFilter.php
+++ app/Filters/StyleFilter.php
@@ -9,7 +9,6 @@ class StyleFilter
     public function filter($builder, $value)
     {
         return $builder->whereHas('style', function ($query) use ($value) {
-            dd($value);
             $query->where('name', 'like', $value);
         });
     }
