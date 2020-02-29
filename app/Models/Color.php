6b443b08e71c52adad17212d021c1ea0ddbaf53c
imsidz <imsidzluv@gmail.com>
2020-03-01 04:41:59 +0530

    fix colors

--- app/Models/Color.php
+++ app/Models/Color.php
@@ -8,4 +8,9 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Color extends Model
 {
     use SoftDeletes;
+
+    public function products()
+    {
+        return $this->belongsToMany(Product::class, 'color_product', 'color_id', 'product_id')->withTimestamps();
+    }
 }
